32a37f3aeed96262c2f2b6b3036ec414493a3b19
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2024-10-02 17:49:18 -0400

    Add for loop to position boxs
    The boxes are supposed to be in a 16x 16 grid

--- javascript.js
+++ javascript.js
@@ -1,9 +1,26 @@
 let GridContainer = document.createElement("div")
+GridContainer.setAttribute("style", "display: flex")
+let NewColumn
 document.body.appendChild(GridContainer)
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < 256; i++){
     let NewDiv = document.createElement("div")
-    NewDiv.setAttribute("style", "background: red; height: 50px; width: 50px; border: 2px solid black;")
-    GridContainer.appendChild(NewDiv)
-    console.log("done")
+    NewDiv.setAttribute("style", "background: red; height: 10px; width: 10px; border: 2px solid black;")
+    
+    if (i%16 == 0){
+        NewColumn = document.createElement("div")
+        NewColumn.setAttribute("class", "column")
+        GridContainer.appendChild(NewColumn)
+        NewColumn.appendChild(NewDiv)
+        NewColumn.setAttribute("style", "border: 2px dotted blue")
+        console.log("new colum ran")
+    } else {
+        NewColumn.appendChild(NewDiv)
+        console.log("old colum ran")
+
+    }
+
+
+
+
 }
